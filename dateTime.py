96a9437aedb47a90b47639bdcb952df81cf4e5c4
caleote <33180406+caleote@users.noreply.github.com>
2017-11-15 22:55:21 +0000

    Update dateTime.py

--- dateTime.py
+++ dateTime.py
@@ -6,8 +6,8 @@
 def get_hours(hm):
     """
     Get the number of hours from a HH:MM time representation.
-    @requires: hm str with a time represented as HH:MM.
-    @ensures: int with the number of hours.
+    Requires: hm str with a time represented as HH:MM.
+    Ensures: int with the number of hours.
     """
     return int(hm.split(':')[0])
 
@@ -15,8 +15,8 @@ def get_hours(hm):
 def get_minutes(hm): #Catarina
     """
     Get the number of minutes from a HH:MM time representation.
-    @requires: hm str with a time represented as HH:MM.
-    ´@ensures: int with the number of minutes.
+    Requires: hm str with a time represented as HH:MM.
+    Ensures: int with the number of minutes.
     """
     return int(hm.split(':')[1])
 
@@ -24,10 +24,10 @@ def get_minutes(hm): #Catarina
 def add_minutes(hm, incr): #Catarina
     """
     Increment the given time by the given amount of minutes.
-    @requires:
+    Requires:
     - hm str with a time represented as HH:MM;
     - incr int with the number of minutes.
-    @ensures: str with a time represented as HH:MM, the result of incrementing hm by incr minutes.
+    Ensures: str with a time represented as HH:MM, the result of incrementing hm by incr minutes.
     """
     hours = get_hours(hm)
 	minutes = get_minutes(hm)
