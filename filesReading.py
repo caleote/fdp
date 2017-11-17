37d6552eb11921c93ff294ad81d994fba974eff6
martimseco <m.seco@campus.fct.unl.pt>
2017-11-17 21:53:01 +0000

    implemented the reading of operators file. Added tests

--- filesReading.py
+++ filesReading.py
@@ -58,7 +58,8 @@ def read_requests_file(file_name):
     for line in in_file:
         name, language, domain, service, duration = line.strip().split(', ')
         duration = int(duration)
-        requests.append((name, language, domain, service, duration))
+        requests.append(
+        {name, language, domain, service, duration))
     in_file.close()
     return requests
 
