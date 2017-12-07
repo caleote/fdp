ee4c982a30d77c57bf35a0d8aaba610924f0134e
caleote <33180406+caleote@users.noreply.github.com>
2017-12-07 18:58:17 +0000

    Já com os tuplos

--- filesReading.py
+++ filesReading.py
@@ -6,11 +6,12 @@
 
 import constants
 
-def read_operators_file(file_name): # Martim
+def read_operators_file(file_name):  # Martim
     """
     Read a file with operators into a collection.
     Requires: file_name, str with the name of a text file with a list of operators.
-    Ensures: list, with the requests in the file; each request is a tuple with the various element concerning that request, in the order provided in the file.
+    Ensures: list, with the operators in the file; each operator is a tuple with the
+    various elements, in the order provided in the file.
     """
     in_file = open(file_name, 'r')
     for i in range(constants.HEADER_TOTAL_LINES):  # read some lines to skip the header
@@ -20,29 +21,17 @@ def read_operators_file(file_name): # Martim
     for line in in_file:
         name, language, domains, hourFinish, minutesDone = line.strip().split(', ')
         domains = domains[1:-1].split('; ')
-        operators.append(
-        {'name':name,
-        'language':language,
-        'domains':domains,
-        'hourFinish':hourFinish,
-        'minutesDone':minutesDone
-         })
+        operators.append((name, language, domains, hourFinish, minutesDone))
     in_file.close()
 
     return operators
-
-o = read_operators_file('operators15h00.txt')
-print('o type:', type(o))
-print('o:', o)
-#print(o.values())
-
 
 def read_requests_file(file_name):
     """
     Read a file with requests into a collection.
     Requires: file_name, str with the name of a text file with a list of requests.
-    Ensures: list, with the requests in the file; each request is a tuple with the various element
-    concerning that request, in the order provided in the file.
+    Ensures: list, with the requests in the file; each request is a tuple with the
+    various element concerning that request, in the order provided in the file.
     """
     in_file = open(file_name, 'r')
     for i in range(constants.HEADER_TOTAL_LINES):  # read some lines to skip the header
@@ -52,13 +41,7 @@ def read_requests_file(file_name):
     for line in in_file:
         name, language, domain, service, duration = line.strip().split(', ')
         duration = int(duration)
-        requests.append(
-        {'name':name,
-         'language':language,
-         'domain':domain,
-         'service':service,
-         'duration':duration
-         })
+        requests.append((name, language, domain, service, duration))
     in_file.close()
-    return requests
 
+    return requests
