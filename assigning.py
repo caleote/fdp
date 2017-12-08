b0ef0af886bff02e133f2244e2cafd3c41c76b32
caleote <fc51705@alunos.fc.ul.pt>
2017-12-08 16:19:19 +0000

    findMatchingOperators mal

--- assigning.py
+++ assigning.py
@@ -6,6 +6,7 @@
 
 from dateTime import *
 from filesReading import *
+from constants import *
 
 def isPremium(request): #Catarina
     """
@@ -41,7 +42,6 @@ def assign_tasks(operators, requests, current_time): #Catarina Martim
         'hourFinish':oplist[t][3],
         'minutesDone':oplist[t][4]
          })
-
     reqlist = read_requests_file(requests)
     reqdict = []
     for t in range (len(reqlist)):
@@ -52,18 +52,18 @@ def assign_tasks(operators, requests, current_time): #Catarina Martim
          'service':reqlist[t][3],
          'duration':reqlist[t][4]
          })
+
     assignments = []
     fremiumList = []
     for req in reqdict:
         if isPremium(req) == True:
             #encontrar operador match
-            op = findMatchingOperator(opdict, req['language'], req['domain'], current_time)
+            op = findMatchingOperator(opdict, req['duration'], req['language'], req['domain'], current_time)
 
             if op != None:
                 start_time = max_time(current_time, op['hourFinish'])
 
                 assignment = {'operator': op['name'], 'client': req['name'], 'time': start_time}
-                op['minutesDone'] = int(op['minutesDone']) + req['duration']
                 op['hourFinish'] = add_minutes(current_time, req['duration'])
             else:
                 assignment = {'operator': 'not-assigned', 'client': req['name'], 'time': current_time}
@@ -73,11 +73,10 @@ def assign_tasks(operators, requests, current_time): #Catarina Martim
             fremiumList.append(req)
 
     for fre in fremiumList:
-        op = findMatchingOperator(opdict, fre['language'], fre['domain'], current_time)
+        op = findMatchingOperator(opdict, fre['duration'], fre['language'], fre['domain'], current_time)
         if op != None:
             start_time = max_time(current_time, op['hourFinish'])
             assignment = {'operator': op['name'], 'client': fre['name'], 'time': start_time}
-            op['minutesDone'] = int(op['minutesDone']) + fre['duration']
             op['hourFinish'] = add_minutes(current_time, fre['duration'])
         else:
             assignment = {'operator': 'not-assigned', 'client': fre['name'], 'time': current_time}
@@ -85,7 +84,40 @@ def assign_tasks(operators, requests, current_time): #Catarina Martim
     # When all assignments are done :
     return assignments
 
-def findMatchingOperator(operators, language, domain, time): # Martim
+def minMinutes(minutes1, minutes2):
+    '''
+    #TODO preencher
+    :param minutes1:
+    :param minutes2:
+    :return:
+    '''
+    minutes1 = int(minutes1)
+    minutes2 = int(minutes2)
+    if minutes1 <= minutes2:
+        return minutes1
+    else:
+        return minutes2
+
+def firstInAlphabet(word1, word2):
+    '''
+    #TODO preencher
+    SE HOUVER TEMPO CORRIGIR ISTO PARA O CASO DA ANA E DA ANABELA
+    :param word:
+    :return:
+    '''
+    if word1 == word2:
+        return word1
+    else:
+        for l in range (len(word1)):
+            if word1[l] == word2[l]:
+                l += 1
+            else:
+                if word1[l] < word2[l]:
+                    return word1
+                else:
+                    return word2
+
+def findMatchingOperator(operators, duration, language, domain, time): # Martim
     '''
     TODO preencer
     :param operators:
@@ -99,9 +131,25 @@ def findMatchingOperator(operators, language, domain, time): # Martim
     # Ordena-se primeiro... faltam os desempates e a reordenação depois de um update
 
     for op in operators:
-        if op['language'] == language and domain in op['domains']:
-            return op
+        for op2 in operators:
+            if op2 != op:
+                if op['language'] == language and domain in op['domains']:
+                    print('op14:', op['name'])
+                    print('op24:', op2['name'])
+                    if op['hourFinish'] == str(min_time(op['hourFinish'], op2['hourFinish'])):
+                        print('op15:', op['name'])
+                        print('op25:', op2['name'])
+                        if op['minutesDone'] == str(minMinutes(op['minutesDone'], op2['minutesDone'])):
+                            op['minutesDone'] = int(op['minutesDone']) + duration
+                            if op['minutesDone'] <= 240:
+                                if op['name'] == firstInAlphabet(op['name'], op2['name']):
+                                    return op
     return None
+
+operators = [('Tiago','french','laptops', '16:50','113'), ('Helena','french', 'laptops', '16:57', '198')]
+ass = assign_tasks('operators16h55.txt', 'requests16h55.txt', '17:00')
+print ('ass:', ass)
+
 
 def headername(file_name): #Catarina
     in_file = open(file_name, 'r')
@@ -110,3 +158,4 @@ def headername(file_name): #Catarina
     typeoffile = (lines[6])[:-2]
     in_file.close()
     return time, typeoffile
+
